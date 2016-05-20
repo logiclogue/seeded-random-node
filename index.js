04ce9e804458d96dd5234e2fd7a88aba1fd8d297
logiclogue <jordanpaullord@hotmail.co.uk>
2016-05-20 21:47:05 +0100

    Add Command class

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 #!/usr/bin/env node
 
 var Random = require('./Random');
+var Command = require('./Command');
 var random = new Random();
-var args = process.argv.slice(2);
+var command = new Command();
 
-
-switch(args[0]) {
-    case 'decimal':
-        console.log(random.decimal(args[1]));
-        break;
-
-    case 'range':
-        console.log(random.range(args[1], parseInt(args[2]), parseInt(args[3])));
-        break;
-
-    default:
-        console.log('Not a valid function');
-        break;
-}
 
 module.exports = {
     Random: Random,
